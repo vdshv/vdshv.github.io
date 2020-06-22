489e65e7bab22466ac132cde50c9ebd44fc16b8d
Vadym Shevchenko <vadik.shevchenko@gmail.com>
2020-06-22 17:44:05 +0300

    js upd

--- typesy/site-assets/js/index.js
+++ typesy/site-assets/js/index.js
@@ -22,20 +22,16 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 
+
+    PostAffTracker.setAccountId('default1');
+    try {
+    PostAffTracker.disableTrackingMethod('F');
+     
+    PostAffTracker.track();
+     
+    } catch (err) { }
+
     
-	// document.write(decodeURI("%3Cscript id='pap_x2s6df8d' src='" + (("https:" == document.location.protocol) ? "https://" : "http://") +  
-	// "ereflect.postaffiliatepro.com/scripts/eqyyk4n7jm2' type='text/javascript'%3E%3C/script%3E"));
-
-
-	// PostAffTracker.setAccountId('default1');
-	// try {
-	// PostAffTracker.disableTrackingMethod('F');
-	 
-	// PostAffTracker.track();
-	 
-	// } catch (err) { }
-
-
 	var ck = "https://www.typesy.com/";
 
 	jQuery(document).ready(function(){    
