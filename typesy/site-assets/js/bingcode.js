0ef516c34463bcc51f7faa4e728fd45fdd2d4d99
Vadym Shevchenko <vadik.shevchenko@gmail.com>
2020-07-23 12:54:31 +0300

    delete extra files

--- typesy/site-assets/js/bingcode.js
+++
@@ -1,4 +0,0 @@
-// JavaScript Document
-
-<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"4021847"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");</script><noscript><img src="//bat.bing.com/action/0?ti=4021847&Ver=2" height="0" width="0" style="display:none; visibility: hidden;" /></noscript>
-<script type="text/javascript">var $zoho= $zoho || {salesiq:{values:{},ready:function(){}}};var d=document;s=d.createElement("script");s.type="text/javascript";s.defer=true;s.src="https://salesiq.zoho.com/ereflect1/float.ls?embedname=7speedreading";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);</script>
