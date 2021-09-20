d74fc26fb17bcac7e22a0059d836e09c551efc48
Vadym Shevchenko <vadik.shevchenko@gmail.com>
2021-09-20 13:57:10 +0300

    form ajax

--- consovenio/app/js/common.js
+++ consovenio/app/js/common.js
@@ -51,133 +51,81 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 	
+	var form = qs('.contact form'),
+		formCont = qs('.contact.form'),
+		error = qs('.contact__error');
 
-    $('.cloock.sender').click(function () {
+    form.onsubmit = (e) => {
+    	e.preventDefault();
 
-        var canSend = false;
-        var dialog = '';
+    	var canSend = false,
+    		dialog = '',
+			email = qs('[type=email]').value,
+        	name = qs('[type=text]').value,
+        	comment = qs('textarea').value;
 
-        console.log('contact form - Btn click');
-
-        var email = $("input.input-email").val();
-        var name = $("input.input-name").val();
-        var comment = $("textarea.text-comment").val();
-
-        console.log('contact form - email: ' + email);
-        console.log('contact form - name: ' + name);
-        console.log('contact form - comment: ' + comment);
-
-
-        if (email.length === 0) {
-            dialog = "Oops, you are missing a required field. ";
-            canSend = false;
-
-
+        if (email.length == 0) {
+            dialog = "Please, fill in your email.";
         } else {
-
             var validation = isMailValid(email);
-            console.log('validation: ' + validation);
 
             if (!validation) {
-
-
-                dialog = "You have entered an invalid e-mail address. Please try again. ";
-                canSend = false;
-
-
+                dialog = "You have entered an invalid e-mail address. Please try again.";
+            } else if (!name.length) {
+                dialog = "Please, fill in your name/company.";
+            } else if (!comment.length) {
+                dialog = "Please, fill in your message to us.";
             } else {
                 canSend = true;
-
-                if (name.length === 0) {
-                    dialog = "Oops, you are missing a required field. ";
-                    canSend = false;
-
-                } else {
-                    canSend = true;
-
-                    if (comment.length === 0) {
-                        dialog = "Oops, you are missing a required field. ";
-                        canSend = false;
-                    } else {
-                        canSend = true;
-                    }
-
-                }
-
             }
-
         }
 
-       
-
-       
-
-
         if (canSend) {
-
-            jsonItem = {};
+            var jsonItem = {};
             jsonItem["name"] = name;
             jsonItem["email"] = email;
             jsonItem["comment"] = comment;
 
-            $.ajax({
-                type: "POST",
-                url: "https://pdconn.magnusco.net/api/IncomingMessages",
-                data: JSON.stringify(jsonItem),
-                contentType: 'application/json',
-                success: function (res) {
-
-                    var $container = $("html,body");
-                    var $scrollTo = $('.cont-intro');
-
-                    $container.animate({
-                        scrollTop: $scrollTo.offset().top,
-                        scrollLeft: 0
-                    }, 450);
-
-                    //console.log(res);
-                    console.log("Added");
-
-                    $('.js-contact-box').fadeOut();
-                    dialog = "Thank you for getting in touch! We appreciate you contacting Consovenio. One of our colleagues will get back in touch with you soon! Have a great day!";
-
-
-                    $('.contact-box-dialog').hide();
-                    $('.contact-box-dialog').removeClass('error').addClass('success');
-                    $('.contact-box-dialog').text(dialog);
-                    $('.contact-box-dialog').fadeIn("slow");
-
-
-                }.bind(this),
-                error: function (xhr, status, err) {
-                    console.error(xhr, status, err.toString());
-                }.bind(this)
+            fetch('https://pdconn.magnusco.net/api/IncomingMessages', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(jsonItem),
+            })
+            .then(response => {
+            	dialog = "Thank you for getting in touch! We appreciate you contacting Consovenio. One of our colleagues will get back in touch with you soon! Have a great day!";
+            	error.classList.add('active');
+            	error.innerHTML = dialog;
+            	error.style.height = '50px';
+            	window.scrollTo({
+            	    top: offsetTop(error) - 50,
+            	    behavior: "smooth"
+            	});
+            })
+            .catch((error) => {
+              console.error('Error:', error);
             });
 
         } else {
-
-            var $container = $("html,body");
-            var $scrollTo = $('.cont-intro');
-
-            $container.animate({
-                scrollTop: $scrollTo.offset().top,
-                scrollLeft: 0
-            }, 450);
-
-
-            $('.contact-box-dialog').hide();
-            $('.contact-box-dialog').removeClass('success').addClass('error');
-            $('.contact-box-dialog').text(dialog);
-            $('.contact-box-dialog').fadeIn("slow");
-
+        	error.classList.add('active');
+        	error.innerHTML = dialog;
+        	error.style.height = '50px';
+        	window.scrollTo({
+        	    top: offsetTop(error) - 50,
+        	    behavior: "smooth"
+        	});
         }
+    }
 
-    });
+    function offsetTop(el) {
+        var rect = el.getBoundingClientRect(),
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return rect.top + scrollTop;
+    }
 	function isMailValid(email) {
-
 	    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 	    return emailReg.test(email);
-
 	}
 
 
